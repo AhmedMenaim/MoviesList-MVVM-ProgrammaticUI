019b19d95ff4a7c7a53797d41d87c8e3256c5ca6
AhmedMenaim <ahmedmenaim8@gmail.com>
2024-07-31 22:52:32 +0300

    Initial Commit

---
+++ MoviesList-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesList-MVVM
+//
+//  Created by Menaim on 31/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
