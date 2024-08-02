a590b78d1adbc0ebfcd17fe691c756aa85305670
AhmedMenaim <ahmedmenaim8@gmail.com>
2024-08-02 21:15:40 +0300

    feat(movies) 🚧 Add the domain layer.

---
+++ MoviesList-MVVM/Modules/Movies/Domain/UseCase/MoviesUseCase.swift
@@ -0,0 +1,85 @@
+//
+//  MoviesUseCase.swift
+//  MoviesList-MVVM
+//
+//  Created by Menaim on 02/08/2024.
+//
+
+import Foundation
+
+final class MoviesUseCase {
+  // MARK: - Dependencies
+  private var moviesRepository: MoviesRepositoryProtocol
+  private var genresRepository: GenreRepositoryProtocol
+
+  init(
+    moviesRepository: MoviesRepositoryProtocol,
+    genresRepository: GenreRepositoryProtocol
+  ) {
+    self.moviesRepository = moviesRepository
+    self.genresRepository = genresRepository
+  }
+
+
+  private func convert(_ repositoryItem: [MovieRepositoryModel]?) async -> [MovieItem] {
+    var genres: [Int?: String?]?
+    do {
+      let genresResult = try await fetchGenres()
+      genres = genresResult
+    } catch {
+      print(error)
+    }
+    guard
+      let movies = repositoryItem, 
+      let genres
+    else {
+      return []
+    }
+    return movies.compactMap({ movie in
+      MovieItem(
+        posterPath: movie.posterPath ?? "",
+        title: movie.title ?? "",
+        releaseDate: movie.releaseDate ?? Date(),
+        genres: fetchStringGenres(genres, movie.genreIDs),
+        id: movie.id ?? 0,
+        voteAverage: movie.voteAverage ?? 0.0,
+        voteCount: movie.voteCount ?? 0,
+        overview: movie.overview ?? ""
+      )
+    })
+  }
+
+  private
+  func convert(_ repositoryItem: MoviesRepositoryModel?) async -> MoviesItems {
+    await MoviesItems(
+      page: repositoryItem?.page ?? 0,
+      movies: convert(repositoryItem?.movies),
+      totalPages: repositoryItem?.totalPages ?? 0
+    )
+  }
+
+  private
+  func fetchStringGenres(_ genreDictionary: [Int?: String?], _ usedIDs: [Int]?) -> [String] {
+    guard let usedIDs else { return [] }
+    return usedIDs.compactMap { genreDictionary[$0] ?? "Default" }
+  }
+
+  private
+  func fetchGenres() async throws -> [Int?: String?] {
+    guard let genres = try await genresRepository.getGenre() else {
+      return [0:""]
+    }
+    return genres
+  }
+}
+
+// MARK: - MoviesUseCaseProtocol
+extension MoviesUseCase: MoviesUseCaseProtocol {
+  func fetchMovies() async throws -> MoviesItems {
+    guard let movies = try await moviesRepository.getMovies() else {
+      return MoviesItems()
+    }
+    print(movies)
+    return await convert(movies)
+  }
+}
---
+++ MoviesList-MVVM/Modules/Movies/Domain/UseCase/MoviesUseCaseProtocol.swift
@@ -0,0 +1,39 @@
+//
+//  MoviesUseCaseProtocol.swift
+//  MoviesList-MVVM
+//
+//  Created by Menaim on 02/08/2024.
+//
+
+import Foundation
+
+protocol MoviesUseCaseProtocol {
+  func fetchMovies() async throws -> MoviesItems
+}
+
+struct MoviesItems {
+  let page: Int
+  let movies: [MovieItem]
+  let totalPages: Int
+
+  init(
+    page: Int = 0,
+    movies: [MovieItem] = [],
+    totalPages: Int = 0
+  ) {
+    self.page = page
+    self.movies = movies
+    self.totalPages = totalPages
+  }
+}
+
+struct MovieItem: Identifiable {
+  let posterPath: String
+  let title: String
+  let releaseDate: Date
+  let genres: [String]
+  let id: Int
+  let voteAverage: Double
+  let voteCount: Int
+  let overview: String
+}
